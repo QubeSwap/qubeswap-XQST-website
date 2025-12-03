be33d15039be3388e8391970f7b6030a47fe083e
HardCarrier <leijia322@gmail.com>
2025-12-03 09:16:42 -0500

    change logo

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -8,8 +8,8 @@ type LogoProps = {
 export const Logo: FC<LogoProps> = ({ size = "text-lg" }) => {
   return (
     <div className={`flex items-center ${size} gap-1 font-audiowide tracking-wide`}>
-      <Image src="/images/FoxLogo.svg" alt="QubeSwapLogo" width={180} height={45} />
-      <span className="font-semibold text-orange-600 "> | XQST</span>
+      <Image src="/images/logo.png" alt="QubeSwapLogo" width={40} height={45} />
+      <span className="text-white">QubeSwap</span><span className="font-semibold text-orange-600 "> | XQST</span>
     </div>
   )
 }
